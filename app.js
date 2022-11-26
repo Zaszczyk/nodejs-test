aab97425891245fb32e6e9def6ef22f75616b47e
Mateusz Błaszczyk <mateusz.blaszczyk@worksmile.com>
2022-11-26 18:24:12 +0100

    develop

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = 80;
 
-io.configure(function() {
-    io.set('transports', ['websocket']);
+io = require('socket.io')(server, {
+    path: '/socket.io'
+});
+
+server.listen(port);
+
+/** ERRORS **/
+process.on('uncaughtException', function (err) {
+    console.log(err);
+});
+
+io.on('error', function (err) {
+    console.log(err);
 });
